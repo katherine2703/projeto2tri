f2c83d4a715f7f8c1d8d68d6edcefb61e8e42802
katherine2703 <112878357+katherine2703@users.noreply.github.com>
2024-07-25 16:38:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal=document.querySelector(¨.caixa-principal¨);
+const caixaPerguntas=document.querySelector(¨.caixa-perguntas¨);
+const caixaAlternativas=document.querySelector(¨.caixa-alternativas¨)
+const caixaResultado=document.querySelector(¨.caixa-resultado¨);
+const textoResultado=document.querySelector(¨.texto-resultado¨);
+
+const Perguntas[
+    {
+        enunciado:¨Você cuida do meio ambiente?¨
+        alternativas:[
+        ¨sim¨
+        ¨não¨
+        ]
+    }
+]
+
+
