33e6009fb90637ed591ae950308ad8423e0d9b8b
katherine2703 <112878357+katherine2703@users.noreply.github.com>
2024-08-01 17:10:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,38 @@ const Perguntas[
         ¨não¨
         ]
     }
-]
 
+    {
+        enunciado:¨Qual a importância das árvores para o meio ambiente?¨
+        alternativas:[
+        ¨ajudam a purificar o ar e contribuem para a manutenção da umidade do solo¨
+        ¨para liberar gás carbônico no planeta e dar frutos¨
+        ]
+    }
+
+    {
+        enunciado:¨como as sacolas plásticas podem afetar a vida marinha?¨
+        alternativas:[
+        ¨elas não interferem em nada¨
+        ¨os animais marinhos podem confundi-la com alimentos e ingerir em seu organismo, causando a morte do animal¨
+        ]
+    }
+
+    {
+        enunciado:¨como podemos economizar água diariamente?¨
+        alternativas:[
+        ¨demorando no banho, deixando a torneira ligada enquanto escova os dentes¨
+        ¨fechando a torneira ao escovar os dentes, tomar banhos mais curtos, consertar torneiras com vazamentos¨
+        ]
+    }
+
+    {
+        enunciado:¨o que devemos fazer com o óleo de cozinha usado?¨
+        alternativas:[
+        ¨guardar em recepientes adequados e leva-lo até postos de coleta adequados¨
+        ¨jogar no ralo da pia¨
+        ]
+    }
+
+]
 
